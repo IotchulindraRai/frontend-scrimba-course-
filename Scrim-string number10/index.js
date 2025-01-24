eb08d1d1f86c7a4152d39b91ba84b9375158b2a2
Chulindra Rai <chulindrarai123@gmail.com>
2025-01-24 10:57:29 +0545

    First commit

---
+++ Scrim-string number10/index.js
@@ -0,0 +1,11 @@
+let name = 42
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+// console.log(myGreeting)
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = 4 + 
+
+console.log(totalPoints)
